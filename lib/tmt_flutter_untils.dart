756752cae6d68e431b2731326979130834706f78
namnv <nam.nvnnb@gmail.com>
2020-01-02 09:27:26 +0700

    -add Sring extension

--- lib/tmt_flutter_untils.dart
+++ lib/tmt_flutter_untils.dart
@@ -4,3 +4,4 @@ export 'sources/datetime_until/datetime_until.dart';
 export 'sources/datetime_until/datetime_extensions.dart';
 export 'sources/datetime_until/datetime_range_type.dart';
 export 'sources/datetime_until/datetime_range.dart';
+export 'sources/string_until/string_extensions.dart';
