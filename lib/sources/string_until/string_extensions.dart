c09c5074070ac9ebe0e2a10fd43e5447d2a58e4d
namnv <nam.nvnnb@gmail.com>
2020-01-02 09:28:37 +0700

    -add Sring extension

--- lib/sources/string_until/string_extensions.dart
+++ lib/sources/string_until/string_extensions.dart
@@ -2,4 +2,8 @@ extension StringExtensions on String {
   bool isNullOrEmply() {
     return (this == null || this.isEmpty);
   }
+
+  bool isNotNullOrEmpty() {
+    return (this != null && this.isNotEmpty);
+  }
 }
