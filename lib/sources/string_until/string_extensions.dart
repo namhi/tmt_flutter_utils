6097ce5429084fc49b4a227465c1762f1740d6cc
namnv <nam.nvnnb@gmail.com>
2020-03-12 17:59:05 +0700

    - add Convert to Enum from String

--- lib/sources/string_until/string_extensions.dart
+++ lib/sources/string_until/string_extensions.dart
@@ -1,9 +1,16 @@
+import 'package:flutter/foundation.dart';
+
 extension StringExtensions on String {
-  bool isNullOrEmply() {
+  bool isNullOrEmpty() {
     return (this == null || this.isEmpty);
   }
 
   bool isNotNullOrEmpty() {
     return (this != null && this.isNotEmpty);
+  }
+
+  T toEnum<T>(Iterable<T> values) {
+    return values.firstWhere((f) => f.toString().split('.').last == this,
+        orElse: () => null);
   }
 }
