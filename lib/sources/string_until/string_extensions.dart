c924a454f637752d33e5875b471efefe90ca9c24
namnv <nam.nvnnb@gmail.com>
2020-03-27 17:34:10 +0700

    - add string extension

--- lib/sources/string_until/string_extensions.dart
+++ lib/sources/string_until/string_extensions.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/foundation.dart';
+import 'dart:convert';
+import 'package:tmt_flutter_untils/sources/regex_pattern.dart';
+import 'package:crypto/crypto.dart';
+import 'package:diacritic/diacritic.dart';
 
 extension StringExtensions on String {
   bool isNullOrEmpty() {
@@ -13,4 +16,24 @@ extension StringExtensions on String {
     return values.firstWhere((f) => f.toString().split('.').last == this,
         orElse: () => null);
   }
+
+  bool isEmail() {
+    return RegExp(emailPattern).hasMatch(this);
+  }
+
+  bool isPhoneNumber() => RegExp(phonePattern).hasMatch(this);
+  bool isBase64() => RegExp(base64Pattern).hasMatch(this);
+  String encryptMd5() => md5.convert(utf8.encode(this)).toString();
+  String encodeToBase64() {
+    Codec<String, String> stringToBase64 = utf8.fuse(base64);
+    return stringToBase64.encode(this);
+  }
+
+  String decodeFromBase64() {
+    Codec<String, String> stringToBase64 = utf8.fuse(base64);
+    return stringToBase64.decode(this);
+  }
+
+  String removeVietnameseMark({bool toLower = false}) =>
+      toLower ? removeDiacritics(this.toLowerCase()) : removeDiacritics(this);
 }
