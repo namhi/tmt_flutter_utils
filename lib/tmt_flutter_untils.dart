e0aef249ec9b841a060faa4ccc5a2bd52c108eac
namnv <namnv@tmtsofts.com>
2020-08-11 08:24:18 +0700

    - add collection extension

--- lib/tmt_flutter_untils.dart
+++ lib/tmt_flutter_untils.dart
@@ -6,3 +6,4 @@ export 'sources/datetime_until/datetime_range_type.dart';
 export 'sources/datetime_until/datetime_range.dart';
 export 'sources/string_until/string_extensions.dart';
 export 'sources/num_until/num_extensions.dart';
+export 'sources/collection_util/collection_extensions.dart';
