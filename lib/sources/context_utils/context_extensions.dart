806c01f8a7c8a325ca68065b432a1e4a56fa272b
namnv <namnv@tmtsofts.com>
2020-09-28 11:08:51 +0700

    - add extensions

---
+++ lib/sources/context_utils/context_extensions.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+extension ContextExtensions on BuildContext {
+  Size get mediaQuerySize => MediaQuery.of(this).size;
+  double get height => mediaQuerySize.height;
+  double get width => mediaQuerySize.width;
+
+  ThemeData get theme => Theme.of(this);
+  TextTheme get textTheme => theme.textTheme;
+
+  EdgeInsets get mediaQueryViewInsets => MediaQuery.of(this).viewInsets;
+  EdgeInsets get mediaQueryViewPadding => MediaQuery.of(this).viewPadding;
+  double get mediaQueryShortestSide => mediaQuerySize.shortestSide;
+
+  bool get isPhone => mediaQueryShortestSide < 660;
+  bool get isTablet => mediaQueryShortestSide >= 720;
+
+  /// Get widget of T base on  media query size
+  T responsiveWidget<T>({T mobile, T tablet, T desktop}) {
+    double width = mediaQuerySize.shortestSide;
+    if (Platform.isWindows) {
+      width = mediaQuerySize.width;
+    }
+
+    if (width >= 1200 && desktop != null) {
+      return desktop;
+    }
+    if (width >= 600 && tablet != null) {
+      return tablet;
+    }
+    return mobile;
+  }
+}
--- lib/tmt_flutter_extensions.dart
+++ lib/tmt_flutter_extensions.dart
@@ -2,3 +2,4 @@ export 'sources/string_utils/string_extensions.dart';
 export 'sources/collection_util/collection_extensions.dart';
 export 'sources/num_utils/num_extensions.dart';
 export 'sources/datetime_utils/datetime_extensions.dart';
+export 'sources/context_utils/context_extensions.dart';
