b607f0819fd90954bd326eebc0a8cc03e253c853
namnv <namnv@tmtsofts.com>
2020-10-02 22:35:51 +0700

    - add color_utils.dart

---
+++ lib/sources/color_utils/color_utils.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class ColorUtils {
+  ColorUtils._();
+  static Color fromHex(String hexString, [Color defaultValue]) {
+    try {
+      final buffer = StringBuffer();
+      if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
+      buffer.write(hexString.replaceFirst('#', ''));
+      return Color(int.parse(buffer.toString(), radix: 16));
+    } catch (e) {
+      return defaultValue;
+    }
+  }
+}
