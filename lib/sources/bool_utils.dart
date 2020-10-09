d4f88d1ccea68eacb9a090b39a63b9c8089bbb00
namnv <namnv@tmtsofts.com>
2020-10-09 10:17:58 +0700

    - add bool

--- lib/sources/bool_utils.dart
+++ lib/sources/bool_utils.dart
@@ -2,4 +2,11 @@ extension BoolExtensions on bool {
   String toCustomString(String trueValue, String falseValue) {
     return this ? trueValue : falseValue;
   }
+
+  bool getValue() {
+    if (this == null) {
+      return false;
+    }
+    return this;
+  }
 }
--- lib/tmt_flutter_extensions.dart
+++ lib/tmt_flutter_extensions.dart
@@ -4,4 +4,3 @@ export 'sources/num_utils/num_extensions.dart';
 export 'sources/datetime_utils/datetime_extensions.dart';
 export 'sources/context_utils/context_extensions.dart';
 export 'sources/bool_utils.dart';
-
