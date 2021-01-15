8fa8922a9d3c4d5b78137cd3a6eed7e435ad1e8d
Nguyen Van Nam <nam.nvnnb@hotmail.com>
2021-01-15 22:43:32 +0700

    - quick fix

--- lib/sources/widget_extensions.dart
+++ lib/sources/widget_extensions.dart
@@ -2,13 +2,22 @@ import 'package:flutter/material.dart';
 
 extension WidgetExtensions on Widget {
   Widget padding(
-      {double all, double left, double top, double right, double bottom}) {
+      {double all,
+      double left,
+      double top,
+      double right,
+      double bottom,
+      Key key}) {
     if (all != null) {
       return Padding(padding: EdgeInsets.all(all));
     }
     return Padding(
+      key: key,
       padding:
           EdgeInsets.only(left: left, top: top, right: right, bottom: bottom),
+      child: this,
     );
   }
+
+  Widget sliverToBox({Key key}) => SliverToBoxAdapter(child: this, key: key);
 }
