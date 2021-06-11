987456310888fbd7d04452178b2078eb06d31ad3
Nguyen Van Nam <namnv@tmtsofts.com>
2021-06-11 15:26:58 +0700

    - Add byteSizeToString to IntUtils

--- lib/sources/num_utils/int_extensions.dart
+++ lib/sources/num_utils/int_extensions.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 extension IntExtensions on int {
@@ -15,4 +17,14 @@ extension IntExtensions on int {
   Duration get hour => Duration(hours: this);
   Duration get minute => Duration(minutes: this);
   Duration get second => Duration(seconds: this);
+
+  /// Returns text representation of a provided bytes value
+  /// e.g 1Kb, 12MB, 2GB)...
+  String byteSizeToString([int fractionDigits = 2]) {
+    if (this <= 0) return '0 B';
+    final multiple = (log(this) / log(1024)).floor();
+    return (this / pow(1024, multiple)).toStringAsFixed(fractionDigits) +
+        ' ' +
+        ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'][multiple];
+  }
 }
