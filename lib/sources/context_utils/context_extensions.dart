194965b86b400d704e3444b08a86a5544762ee68
Nguyen Van Nam <namnv@tmtsofts.com>
2021-06-19 09:21:29 +0700

    Edit context_extensions.dart

--- lib/sources/context_utils/context_extensions.dart
+++ lib/sources/context_utils/context_extensions.dart
@@ -14,13 +14,21 @@ extension ContextExtensions on BuildContext {
   EdgeInsets get mediaQueryViewPadding => MediaQuery.of(this).viewPadding;
   double get mediaQueryShortestSide => mediaQuerySize.shortestSide;
 
+  /// Return true if screen current is phone.
   bool get isPhone => mediaQueryShortestSide < 660;
-  bool get isTablet => mediaQueryShortestSide >= 720;
+  bool get isSmallTablet => mediaQueryShortestSide >= 600;
+  bool get isLargeTablet => mediaQueryShortestSide >= 720;
+
+  /// Return true if screen current is tablet.
+  bool get isTablet => isSmallTablet || isLargeTablet;
   bool get isKeyboardOpen => MediaQuery.of(this).viewInsets.bottom != 0;
 
   /// Get widget of T base on  media query size
+  /// Return [mobile], [desktop] and [tablet]
+  /// Return default mobile if one of theme is null.
   T? responsiveWidget<T>({T? mobile, T? tablet, T? desktop}) {
     double width = mediaQuerySize.shortestSide;
+
     if (Platform.isWindows) {
       width = mediaQuerySize.width;
     }
