b2274c56bfe485eb2d77f42e031d7462fe1fa8ed
Nguyen Van Nam <namnv@tmtsofts.com>
2021-08-07 21:57:54 +0700

    edit text_editing_controller

--- lib/sources/widget_extensions.dart
+++ lib/sources/widget_extensions.dart
@@ -19,11 +19,44 @@ extension WidgetExtensions on Widget {
     );
   }
 
+  Padding paddingLeft({Key? key, required double value}) {
+    return Padding(
+      padding: EdgeInsets.only(left: value),
+      key: key,
+      child: this,
+    );
+  }
+
+  Padding paddingTop({Key? key, required double value}) {
+    return Padding(
+      padding: EdgeInsets.only(top: value),
+      key: key,
+      child: this,
+    );
+  }
+
+  Padding paddingRight({Key? key, required double value}) {
+    return Padding(
+      padding: EdgeInsets.only(right: value),
+      key: key,
+      child: this,
+    );
+  }
+
+  Padding paddingBottom({Key? key, required double value}) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: value),
+      key: key,
+      child: this,
+    );
+  }
+
+  /// Wrap widget by [SliverToBoxAdapter] when using a widget on Sliver and CustomScrollView
   Widget sliverToBox({Key? key}) => SliverToBoxAdapter(child: this, key: key);
 }
 
 extension TextEditingControllerEx on TextEditingController {
-  /// Select all text from textfield.
+  /// Select all text from the TextController.
   void selectAll() {
     this.selection =
         TextSelection(baseOffset: 0, extentOffset: this.text.length);
