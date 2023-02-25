d2002ff5a35c5a33b5a86b64c31fcce1b81cf67d
namnv <vnamct@gmail.com>
2023-02-26 01:25:45 +0700

    FEAT: edit tmt_flutter_extensions.dart

--- lib/tmt_flutter_extensions.dart
+++ lib/tmt_flutter_extensions.dart
@@ -1,9 +1,7 @@
 library tmt_flutter_utils;
 
-import 'package:tmt_dart_utils/tmt_dart_extensions.dart';
-
+export 'package:tmt_dart_utils/tmt_dart_extensions.dart';
 export 'sources/datetime_utils/datetime_extensions.dart';
 export 'sources/context_utils/context_extensions.dart';
 export 'sources/color_utils/color_extensions.dart';
 export 'sources/widget_extensions.dart';
-export 'package:tmt_dart_utils/tmt_dart_utils.dart';
